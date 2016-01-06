be4063481e2ca47615027d5d84da18da8db32c8a
Luke Street <lstreet@aquent.com>
2016-01-06 13:55:29 -0500

    Hide mouse cursor during tests

--- NodeBase/entry_point.sh
+++ NodeBase/entry_point.sh
@@ -31,15 +31,17 @@ fi
 SCREEN_PATH="/tmp/screen"
 mkdir -p $SCREEN_PATH
 xvfb-run --server-args="$DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR -fbdir $SCREEN_PATH" \
-  java ${JAVA_OPTS} -Dvideo.xvfbscreen=$SCREEN_PATH \
-    -cp /opt/selenium/selenium-video-node.jar:/opt/selenium/selenium-server-standalone.jar \
-    org.openqa.grid.selenium.GridLauncher \
-    -servlets com.aimmac23.node.servlet.VideoRecordingControlServlet -proxy com.aimmac23.hub.proxy.VideoProxy \
-    -role node \
-    -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
-    ${REMOTE_HOST_PARAM} \
-    -nodeConfig /opt/selenium/config.json \
-    ${SE_OPTS} &
+  bash -c "unclutter -idle 1 &
+    java ${JAVA_OPTS} -Dvideo.xvfbscreen=$SCREEN_PATH \
+      -cp /opt/selenium/selenium-video-node.jar:/opt/selenium/selenium-server-standalone.jar \
+      org.openqa.grid.selenium.GridLauncher \
+      -servlets com.aimmac23.node.servlet.VideoRecordingControlServlet -proxy com.aimmac23.hub.proxy.VideoProxy \
+      -role node \
+      -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
+      ${REMOTE_HOST_PARAM} \
+      -nodeConfig /opt/selenium/config.json \
+      ${SE_OPTS}
+    kill %1" &
 NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
